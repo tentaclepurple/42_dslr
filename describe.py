07c726da028e3971735246debd1cb01947297f7d
Iban Montero <imontero@student.42urduliz.com>
2024-07-22 20:21:17 +0000

    wip

--- describe.py
+++ describe.py
@@ -1,16 +1,45 @@
 import pandas as pd
+from statistics import ft_mean, ft_median, ft_std, ft_sort, ft_quartile 
 
 
-def ft_describe(path):
+class Describe():
+    """
+    Class to describe a dataset like pandas.describe()
+    """
+    def __init__(self, df: pd.DataFrame):
+        """
+        creates a Describe df object
+        """
+        #self.column_names = df.columns.to_list()
+        #self.describe_df = pd.DataFrame(columns=self.column_names)
+        self.describe_df = self.create_df(df)
+    
+    def create_df(df):
+        stats = {}
 
-    df = pd.read_csv(path)
-    print(df)
+        for column in df.columuns:
+                    
+
+
+
+def ft_describe(df):
+    Desc = Describe(df)
+    print(Desc.describe_df)
+
+    
 
 
 
 def main():
     path = "datasets/dataset_test.csv"
-    ft_describe(path)
+    df = pd.read_csv(path)
+
+    #print(df)
+
+    print("PANDAS DESCRIBE:")
+    print(df.describe())
+
+    ft_describe(df)
 
 
 if __name__ == "__main__":
