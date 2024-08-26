0fea3bd58d66aabcc9ea1ef4cf2120fbe66df3e3
johnconh <sevenballs@gmail.com>
2024-08-26 19:36:47 +0200

    logreg_predcit_sofmax.py agregado

---
+++ logreg_predict_softmax.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import sys
+import os
+import pickle
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from clean_utils import fill_missing_with_median, normalize_data
+
+def softmax(z):
+    exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))  # Para estabilidad numérica
+    return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
+def predict_softmax(x, theta):
+
+    if theta.shape == (4, 5):
+        theta = theta.T
+    elif theta.shape != (5, 4):
+        raise ValueError(f"Expected theta to have shape (5, 4), but got {theta.shape}")
+    
+    if x.shape[1] == theta.shape[0] - 1:
+        x = np.insert(x, 0, 1, axis=1)
+    
+    z = x @ theta
+    h = softmax(z)
+    predictions = np.argmax(h, axis=1)
+    return predictions
+
+def predict(df, thetas) -> None:
+    df = df[['Ancient Runes', 'Defense Against the Dark Arts', 'Charms', 'Divination']]
+    x = df.to_numpy()
+
+    predictions = predict_softmax(x, thetas)
+    house_names = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+    predictions_houses = [house_names[pred] for pred in predictions]
+
+    results = pd.DataFrame({
+        'Index': df.index,
+        'Hogwarts House': predictions_houses
+    })
+
+    output_file = 'houses.csv'
+    results.to_csv(output_file, index=False)
+
+if __name__ == '__main__':
+        path = sys.argv[1]
+        df = pd.read_csv(path)
+        df_original = pd.read_csv(path)
+        df_clean = fill_missing_with_median(df_original)
+        df_normalized = normalize_data(df_clean)
+        
+        weights_path = sys.argv[2]
+        if os.path.exists(weights_path):
+            if os.access(weights_path, os.R_OK):
+                with open(weights_path, 'rb') as file:
+                    weights = pickle.load(file)
+        
+        predict(df_normalized, weights)
--- logreg_train_softmax.py
+++ logreg_train_softmax.py
@@ -7,34 +7,18 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from clean_utils import fill_missing_with_median, normalize_data
 
-
 def softmax(z):
     exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))  # Para estabilidad numérica
     return exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
-
-def cost_function_softmax(x, y, theta):
-    m = len(y)
-    z = x @ theta
-    h = softmax(z)
-    # Crear un vector de 1-hot encoding para y
-    y_one_hot = np.eye(h.shape[1])[y]
-    cost = -np.sum(y_one_hot * np.log(h)) / m
-    return cost
-
-
 def gradient_descent(x, y, theta, alpha, iterations):
     m = len(y)
-    cost_history = np.zeros(iterations)
     y_one_hot = np.eye(theta.shape[1])[y]
     for i in tqdm(range(iterations)):
         z = x @ theta
-        h = softmax(z)    
-
+        h = softmax(z)
         theta = theta - (alpha / m) * x.T @ (h - y_one_hot)
-        cost_history[i] = cost_function_softmax(x, y, theta)
-    return theta, cost_history
-
+    return theta
 
 def logistic_regression(df):
     
@@ -51,8 +35,7 @@ def logistic_regression(df):
     
     theta = np.zeros((x.shape[1], (len(classes))))
 
-    
-    thetas, cost_histories = gradient_descent(x, y, theta, alpha, iterations)
+    thetas = gradient_descent(x, y, theta, alpha, iterations)
 
     output_dir = 'weights'
     os.makedirs(output_dir, exist_ok=True)
@@ -60,28 +43,14 @@ def logistic_regression(df):
 
     with open(output_path, 'wb') as f:
         pickle.dump(thetas, f)
-    print(f'Weights saved in "{output_path}".')
-
-    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-    axs = axs.flatten()
-    for i, (c, cost_history) in enumerate(zip(classes, cost_histories)):
-        axs[i].plot(cost_history) 
-        axs[i].set_title(c)
-        axs[i].set_xlabel('Iterations')
-        axs[i].set_ylabel('Cost')
-    plt.tight_layout()
-    plt.savefig('weights/cost_softmax.png')
-
 
 if __name__ == "__main__":
     try:
         path = sys.argv[1]
-        
         df_original = pd.read_csv(path)
         df_clean = fill_missing_with_median(df_original)
         df_normalized = normalize_data(df_clean)
-        logistic_regression(df_normalized)
-        
+        logistic_regression(df_normalized)   
     except Exception as e:
         print(e)
         sys.exit(1)
